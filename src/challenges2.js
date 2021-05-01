429a5c68b701fbe74954b46275df82271461fb54
Renata Nunes <renatanunes231@yahoo.com.br>
2021-05-01 04:36:49 -0300

    Desafio 12 - Concluido

--- src/challenges2.js
+++ src/challenges2.js
@@ -53,13 +53,13 @@ function generatePhoneNumber(numberInput) {
   }
 }
 
-console.log(generatePhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1]));
-
-
-
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function triangleCheck(lineA, lineB, lineC) {
+  if ((lineB + lineC < lineA < Math.abs(lineB - lineC)) && (lineA + lineC < lineB < Math.abs(lineA - lineC)) && (lineA + lineB < lineC < Math.abs(lineA - lineB))) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // Desafio 13
